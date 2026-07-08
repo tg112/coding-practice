33f741a4e3a2a2feb33e75eba6baaa8752cdbd43
Shota Togawa <shotatogawa@Shotas-MacBook-Pro.local>
2026-07-08 11:12:49 +0900

    solved 1662

---
+++ basic/1662_check_if_two_string_arrays_are_equivalent.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+
+class Solution(object):
+    def arrayStringsAreEqual(self, word1, word2):
+        """
+        :type word1: List[str]
+        :type word2: List[str]
+        :rtype: bool
+        """
+
+        return "".join(word1) == "".join(word2)
+        
